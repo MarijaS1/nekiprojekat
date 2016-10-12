d7ebf78e6e467be61b1e48eb452f28e1ce276906
Admin <admin@mac-admin.local>
2016-10-12 18:21:06 +0200

    Initial Commit

---
+++ NoviProjekat/FirstViewController.m
@@ -0,0 +1,29 @@
+//
+//  FirstViewController.m
+//  NoviProjekat
+//
+//  Created by Admin on 10/12/16.
+//  Copyright © 2016 Admin. All rights reserved.
+//
+
+#import "FirstViewController.h"
+
+@interface FirstViewController ()
+
+@end
+
+@implementation FirstViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
